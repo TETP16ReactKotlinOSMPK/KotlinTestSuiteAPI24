bccdf16bddf098270f4c3722e0f266c2a8046e45
osksvn <osksvn@outlook.com>
2020-10-29 20:44:18 +0100

    changes

--- app/src/main/java/com/example/kotlintestsuiteapi24/testScenario1Activity.kt
+++ app/src/main/java/com/example/kotlintestsuiteapi24/testScenario1Activity.kt
@@ -134,6 +134,7 @@ class TestScenario1Activity : AppCompatActivity() {
                     val msg = "Photo capture succeeded: $savedUri"
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     Log.d(TAG, msg)
+                    println(savedUri)
                     // Upload to database here?
                 }
             })
